43202c464c9c1c63192be075f5a40089baa4d054
aqev <jose.ramon.acebes@gmail.com>
2015-11-03 17:05:21 +0100

    Removed listeners
    Removed listeners

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,4 @@
 'use strict';
-/*
-self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
-
-  // Estos valores son estáticos, por lo que hace falta una función que recupere los valores de forma dinámica
-  
-  var title = 'Notificación Push';
-  var body = 'Good news everyone!';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
-});
-
-
-self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
-  // See: http://crbug.com/463146
-  event.notification.close();
-
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
-
-});*/
 
 self.addEventListener('notificationclick', function(event) {
   var url = event.notification.data.url;
